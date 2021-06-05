9ad8ae3245bd2917301b6284eb5c2e55399fb514
lhsthree <lhsthree@gmail.com>
2021-06-05 00:24:08 -0500

    feat: add sign up and log in buttons

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,9 @@ import * as ROUTES from '../constants/routes';
 import FirebaseContext from '../context/firebase';
 
 export default function Header(){
+	const { firebase } = useContext(FirebaseContext);
+	const user = true;
+
 	return(
 		<header className="h-16 bg-white border-b mb-8">
 			<div className="container mx-auto max-width-lg h-full">
@@ -13,6 +16,17 @@ export default function Header(){
                     		<Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
                     			<img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
                     		</Link>
+
+                    		{user ? (
+                    			<div className="">
+                    			<button className="bg-blue-500 text-white w-full rounded h-8 font-bold">sign in</button>
+                    			<Link to={ROUTES.SIGN_UP} className="font-bold">
+	                    	Sign Up
+	                    </Link>
+	                    </div>
+	                    ):(
+                    			<button>sign off</button>
+                    			)}
                     	</h1>
                 	</div>
 				</div>
