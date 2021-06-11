be75d7dc9d8825f10c57ff86de475ba2c64471e4
lhsthree <lhsthree@gmail.com>
2021-06-10 22:04:58 -0500

    fix: change text of buttons from white to visible

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,64 @@
-import React, {useContext} from 'react';
-import {Link} from 'react-router-dom';
-import * as ROUTES from '../constants/routes';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
-export default function Header(){
-	const { firebase } = useContext(FirebaseContext);
-	const user = true;
-
-	return(
-		<header className="h-16 bg-white border-b mb-8">
-			<div className="container mx-auto max-width-lg h-full">
-				<div className="flex justify-between h-full">
-					<div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
-                    	<h1>
-                    		<Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
-                    			<img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
-                    		</Link>
-
-                    		{user ? (
-                    			<div className="">
-                    			<button className="bg-blue-500 text-white w-full rounded h-8 font-bold">sign in</button>
-                    			<Link to={ROUTES.SIGN_UP} className="font-bold">
-	                    	Sign Up
-	                    </Link>
-	                    </div>
-	                    ):(
-                    			<button>sign off</button>
-                    			)}
-                    	</h1>
-                	</div>
-				</div>
-			</div>
-		</header>
-	)
+export default function Header() {
+    const { firebase } = useContext(FirebaseContext);
+    const user = {};
+    
+    return (
+        <header className="h-16 bg-white border-b mb-8">
+            <div className="container mx-auto max-width-lg h-full">
+                <div className="flex justify-between h-full">
+                    <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+                        <h1>
+                            <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
+                                <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12" />
+                            </Link>
+                        </h1>
+                    </div>
+                    <div className="text-gray text-center flex items-center align-items">
+                        {user ? (
+                            <>
+                              <Link to={ROUTES.DASHBOARD} arial-label="Home">
+                                    <p>Dashboard</p>
+                                </Link>
+                                
+                                <button
+                                    type="button"
+                                    title="Sign Out"
+                                    onClick={() => firebase.auth().signOut()}
+                                    onKeyDown={(e) => {
+                                    	if (e.key === 'Enter') {
+                                    		firebase.auth().signOut()
+                                    	}
+                                    }}
+                                    >
+                                    Sign Out
+                                    </button>  
+                            </>
+                        ) : (
+                            <>
+                                <Link to={ROUTES.LOGIN}>
+                                    <button 
+                                        type="button"
+                                        className="bg-blue font-bold text-sm rounded w-20 h-8">
+                                        Log In
+                                    </button>
+                                </Link>
+                                <Link to={ROUTES.SIGN_UP}>
+                                	<button 
+                                        type="button"
+                                        className="bg-blue font-bold text-sm rounded w-20 h-8">
+                                        Sign Up
+                                    </button>
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </header>  
+    );
 }
