eb3edaebd57dfaa5f5842118db5ab05423e48b8d
lhsthree <lhsthree@gmail.com>
2021-06-28 21:11:09 -0500

    feat: added loading skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+import Skeleton from 'react-loading-skeleton';
+
 export default function Timeline() {
-    return (<p>I am the timeline</p>);
+    const photos = [1,2,3,4]
+
+    return (
+        <div className="container col-span-2">
+        {!photos ? (
+            <Skeleton
+                count={4}
+                width={640}
+                height={500}
+                className="mb-5"
+            /> 
+            ) : (
+                photos.map((content) => 
+                    <p>I will be a photo</p>
+                )
+            )}
+        </div>
+        );
 }
