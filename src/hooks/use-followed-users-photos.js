850d4c39c82cefede6c9ff72bff3ab47a4105f00
lhsthree <lhsthree@gmail.com>
2021-07-11 21:38:05 -0500

    add hook for adding photos from profiles

--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
+import { getUserByUserId, getUserFollowedPhotos } from '../services/firebase';
 import UserContext from '../context/user';
 
 export default function useFollowedUsersPhotos() {
@@ -8,7 +9,16 @@ export default function useFollowedUsersPhotos() {
     } = useContext(UserContext);
     
     useEffect(() => {
-        async function getTimelinePhotos() {}
+        async function getTimelinePhotos() {
+            const followingUserIds = await getUserByUserId(userId);
+            let followedUserPhotos = [];
+
+            if (followingUserIds && followingUserIds[0].following.length > 0) {
+                // we need to call a function that will get us the photos
+                followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+                setPhotos(followedUserPhotos);
+            }
+         }
         
         getTimelinePhotos();
     }, [userId]);
