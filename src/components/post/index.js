15a57f65f5b65c7eb0855006934bf1949e068e64
lhsthree <lhsthree@gmail.com>
2021-07-12 21:40:51 -0500

    add styling with tailwind to post

---
+++ src/components/post/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
+import Comments from './comments';
+
+export default function Post({ content }) {
+	return (
+		<div className="rounded col-span-4 bg-white mb-16">
+			<div>I am going to be a styled div!</div>
+		</div>
+	)
+}
