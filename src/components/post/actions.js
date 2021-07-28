fb97ed218fb31195aaa6dd7ec97718f8cc55f8df
lhsthree <lhsthree@gmail.com>
2021-07-27 21:39:28 -0500

    feat: adding comment capability

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -20,7 +20,7 @@ export default function Actions({docId, totalLikes, likedPhoto, handleFocus }) {
 				.doc(docId)
 				.update({
 					likes: toggleLiked ? FieldValue.arrayRemove(userId) : FieldValue.arrayUnion(userId)
-				})
+				});
 
 		setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1))
 	}
@@ -37,7 +37,7 @@ export default function Actions({docId, totalLikes, likedPhoto, handleFocus }) {
                             }
                         }}
                         className={`w-8 mr-4 select-none cursor-pointer ${
-                            toggleLiked ? 'fill-current text-red' : 'text-red'
+                            toggleLiked ? 'fill-current text-red-500' : 'text-black'
                         }`}
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -67,10 +67,10 @@ export default function Actions({docId, totalLikes, likedPhoto, handleFocus }) {
                         tabIndex={0}
                     >
                         <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+	                        strokeLinecap="round"
+	                        strokeLinejoin="round"
+	                        strokeWidth={2}
+	                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                         />
                     </svg>
         		</div>
--- src/context/user.js
+++ src/context/user.js
@@ -1,4 +1,4 @@
 import { createContext } from 'react';
 
-const userContext = createContext(null);
-export default userContext;
+const UserContext = createContext(null);
+export default UserContext;
