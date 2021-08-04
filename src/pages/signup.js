c6f27a14f8af4695812a30bca933d9d1898a24f3
lhsthree <lhsthree@gmail.com>
2021-08-04 14:53:04 -0500

    feat: add suggested users to sidebar

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,71 +1,69 @@
-import React, {useState, useEffect, useContext} from 'react'
-import {Link, useHistory} from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
-import { doesUsernameExist} from '../services/firebase'
+import { doesUsernameExist } from '../services/firebase';
 
 export default function SignUp() {
-	const history = useHistory()
-	const { firebase } = useContext(FirebaseContext)
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+    
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [error, setError] = useState('');
+    const isInvalid = username === '' || fullName === '' || password === '' || emailAddress === '';
 
-	const [ username, setUsername] = useState('');
-	const [ fullName, setFullName ] = useState('');
-	const [ emailAddress, setEmailAddress ] = useState('')
-	const [ password, setPassword ] = useState('');
-	const [ error, setError ] = useState(false);
-	const isInvalid = username === '' || fullName ==='' || password === '' || emailAddress === '';
-	
-	const handleSignUp = async (event) => {
-		event.preventDefault();
-
-		const usernameExists = await doesUsernameExist(username);
-			if (!usernameExists.length) {
-				try {
-				const createdUserResult = await firebase.auth().createUserWithEmailAndPassword(emailAddress, password);
-
-				await createdUserResult.user.updateProfile({
-					displayName: username
-				})
-
-				await firebase.firestore().collection('users').add({
-					userId: createdUserResult.user.uid,
-					userName: username.toLowerCase(),
-					fullName,
-					emailAddress: emailAddress.toLowerCase(),
-					following: [],
-					followers: [],
-					dateCreated: Date.now(),
-				})
-				history.push(ROUTES.DASHBOARD)
-			} catch (error) {
-				setUsername('')
-				setFullName('')
-				setFullName('')
-				setError("that username is already taken, please try another");
-			}
-		}else {
-			setEmailAddress('');
-			setPassword('');
-		}
-	}
-		
-
-	useEffect(() => {
-			document.title = 'Sign Up - Instagram'
-		}, [])
-
-
-	return(
-		<div className="container flex mx-auto max-w-xs items-center h-screen">
+    const handleSignUp = async (event) => {
+        event.preventDefault();
+        
+        const doesUsernameExistResult = await doesUsernameExist(username);
+        if (doesUsernameExistResult && doesUsernameExistResult.length === 0) {
+            try {
+                const createdUserResult = await firebase.auth().createUserWithEmailAndPassword(emailAddress, password);
+                
+                await createdUserResult.user.updateProfile({
+                    displayName: username
+                });
+                
+                await firebase.firestore().collection('users').add({
+                    userId: createdUserResult.user.uid,
+                    username: username.toLowerCase(),
+                    fullName,
+                    emailAddress: emailAddress.toLowerCase(),
+                    following: [],
+                    dateCreated: Date.now()
+                });
+                
+                history.push(ROUTES.DASHBOARD);
+            } catch (error) {
+                setFullName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            }
+        } else {
+            setError('That username is already taken, please try another.');
+        }
+    }
+    
+    useEffect(() => {
+        document.title = 'Sign Up - Instagram';
+    }, []);
+    
+    return (
+        <div className="container flex mx-auto max-w-xs items-center h-screen">
             <div className="flex flex-col">
                 <div className="flex flex-col items-center bg-white p-4 border mb-4">
                     <h1 className="flex justify-center w-full">
                         <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
                     </h1>
-
                     {error && <p className="mb-4 text-xs text-red-500 text-center">{error}</p>}
+                    
                     <form onSubmit={handleSignUp} method="POST">
-					<input
+                        <input
                             aria-label="Enter your username"
                             className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
                             type="text"
@@ -87,7 +85,7 @@ export default function SignUp() {
                             type="text"
                             placeholder="Email address"
                             value={emailAddress}
-                            onChange={({ target }) => setEmailAddress(target.value)}
+                            onChange={({ target }) => setEmailAddress(target.value.toLowerCase())}
                         />
                         <input
                             aria-label="Enter your password"
@@ -98,23 +96,24 @@ export default function SignUp() {
                             onChange={({ target }) => setPassword(target.value)}
                         />
                         <button
-                        	disabled={isInvalid}
-                        	type="submit"
-                        	className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && `cursor-not-allowed opacity-50`}`}
+                            disabled={isInvalid}
+                            type="submit"
+                            className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${ isInvalid && 'cursor-not-allowed opacity-50'
+                            }`}
                         >
-                        	Sign Up
+                            Sign Up
                         </button>
-					</form>
+                    </form>
                 </div>
                 <div className="flex justify-center items-center flex-col w-full bg-white p-4 border">
-                	<p className="text-sm">
-                		Have an account{` `}
-                		<Link to={ROUTES.LOGIN} className="font-bold text-blue">
-                			Login
-                		</Link>
-                	</p>
+                    <p className="text-sm">
+                        Have an account?{` `}
+                        <Link to={ROUTES.LOGIN} className="font-bold text-blue">
+                            Login
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
-	)
+    )
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -25,6 +25,24 @@ export async function getUserByUserId(userId) {
     return user;       
 }
 
+export async function getSuggestedProfiles(userId, following) {
+  let query = firebase.firestore().collection('users');
+
+  if (following.length > 0) {
+    query = query.where('userId', 'not-in', [...following, userId]);
+  } else {
+    query = query.where('userId', '!=', userId);
+  }
+  const result = await query.limit(10).get();
+
+  const profiles = result.docs.map((user) => ({
+    ...user.data(),
+    docId: user.id
+  }));
+
+  return profiles;
+}
+
 export async function getUserFollowedPhotos(userId, followingUserIds) {
     const result = await firebase
         .firestore()
